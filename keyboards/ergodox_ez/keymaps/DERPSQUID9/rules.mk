af2ed5a9097b5c349ab4e98d35f7fb914bb7442e
DERPSQUID9 <DERPSQUID@protonmail.com>
2020-11-14 17:23:30 -0500

    Removed outdated readme, removed redundant options in rules.mk

--- keyboards/ergodox_ez/keymaps/DERPSQUID9/rules.mk
+++ keyboards/ergodox_ez/keymaps/DERPSQUID9/rules.mk
@@ -1,18 +1,13 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 
-# Custom Matrix
-SRC = matrix.c
-
-# Enable Features
+# Enable Desired Features
 LTO_ENABLE = yes           # Enable Link Time Optimization
 DYNAMIC_MACRO_ENABLE = yes # Enable Dynamic Macros
 RGB_MATRIX_ENABLE = yes    # Enable RGB Matrix
 TAP_DANCE_ENABLE = yes     # Enable Tap Dance
 
-# Disable Unused Features
-CONSOLE_ENABLE = no        # Disable debugging output
-COMMAND_ENABLE = no        # Disable command
-RGBLIGHT_ENABLE = no       # Disable RGB Light (RGB Matrix is already in use)
+# Disable Unused/Unwanted Features
+COMMAND_ENABLE = no        # Disable debugging commands
 SWAP_HANDS_ENABLE = no     # Disable Swap Hands
 EXTRAKEY_ENABLE = no       # Disable system and audio control keys.
