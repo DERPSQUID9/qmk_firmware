0d5c78cceb73536f274627b64ef535bf2252f937
DERPSQUID9 <DERPSQUID@protonmail.com>
2021-01-06 23:14:26 -0500

    Made per-key led layout section of Ergodox EZ keymap more user-friendly.

--- keyboards/ergodox_ez/keymaps/Anomalocaridid/keymap.c
+++ keyboards/ergodox_ez/keymaps/Anomalocaridid/keymap.c
@@ -187,63 +187,67 @@ void keyboard_post_init_user(void) {
     rgb_matrix_set_speed_noeeprom(60);
 }
 
+// Macro for formatting per-key LED layouts
+// Numbering intended to match respective keys in LAYOUT_ergodox_pretty
+// And skips non-led keys
 // clang-format off
+#define LED_LAYOUT_ergodox_ez_pretty(               \
+/*  ---- LEFT HAND ----     ---- RIGHT HAND ---- */ \
+    L01,L02,L03,L04,L05,    R01,R02,R03,R04,R05,    \
+    L11,L12,L13,L14,L15,    R11,R12,R13,R14,R15,    \
+    L21,L22,L23,L24,L25,    R21,R22,R23,R24,R25,    \
+    L31,L32,L33,L34,L35,    R31,R32,R33,R34,R35,    \
+    L41,L42,L43,L44,            R42,R43,R44,R45     \
+)                                                   \
+                                                    \
+{                                                   \
+    /* Right Hand */                                \
+    {R01}, {R02}, {R03}, {R04}, {R05},              \
+    {R11}, {R12}, {R13}, {R14}, {R15},              \
+    {R21}, {R22}, {R23}, {R24}, {R25},              \
+    {R31}, {R32}, {R33}, {R34}, {R35},              \
+           {R42}, {R43}, {R44}, {R45},              \
+                                                    \
+    /* Left Hand (Flipped Horizontally) */          \
+    {L05}, {L04}, {L03}, {L02}, {L01},              \
+    {L15}, {L14}, {L13}, {L12}, {L11},              \
+    {L25}, {L24}, {L23}, {L22}, {L21},              \
+    {L35}, {L34}, {L33}, {L32}, {L31},              \
+           {L44}, {L43}, {L42}, {L41},              \
+}
+
 const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
-              // Right hand
-    [FNCT] = {{HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE},
-              {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE},
-              {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0},
-              {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0},
-                            {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0},
+    [FNCT] = LED_LAYOUT_ergodox_ez_pretty(
+        HSV_PURPLE, HSV_PURPLE, HSV_PURPLE, HSV_PURPLE, HSV_PURPLE,        HSV_PURPLE, HSV_PURPLE, HSV_PURPLE, HSV_PURPLE, HSV_PURPLE,
+        HSV_PURPLE, HSV_PURPLE, HSV_PURPLE, HSV_PURPLE, HSV_PURPLE,        HSV_PURPLE, HSV_PURPLE, HSV_PURPLE, HSV_PURPLE, HSV_PURPLE,
+        HSV_PURPLE, HSV_PURPLE, HSV_PURPLE, HSV_PURPLE, HSV_OFF,           HSV_OFF,    HSV_OFF,    HSV_OFF,    HSV_OFF,    HSV_OFF,
+        HSV_OFF,    HSV_OFF,    HSV_OFF,    HSV_OFF,    HSV_OFF,           HSV_OFF,    HSV_OFF,    HSV_OFF,    HSV_OFF,    HSV_OFF,
+        HSV_OFF,    HSV_OFF,    HSV_OFF,    HSV_OFF,                                   HSV_OFF,    HSV_OFF,    HSV_OFF,    HSV_OFF
+    ),
 
-              // Left Hand (flipped)
-              {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE},
-              {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE},
-              {0, 0, 0},    {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE}, {HSV_PURPLE},
-              {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0},
-                            {0, 0, 0},    {0, 0, 0},    {0, 0, 0},    {0, 0, 0}},      
+    [MOUS] = LED_LAYOUT_ergodox_ez_pretty(
+        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,        HSV_OFF,     HSV_OFF,     HSV_OFF,     HSV_OFF,     HSV_OFF,
+        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,        HSV_MAGENTA, HSV_MAGENTA, HSV_MAGENTA, HSV_MAGENTA, HSV_OFF,
+        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,        HSV_PURPLE,  HSV_PURPLE,  HSV_PURPLE,  HSV_PURPLE,  HSV_OFF,
+        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,        HSV_BLUE,    HSV_BLUE,    HSV_BLUE,    HSV_BLUE,    HSV_OFF,
+        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,                              HSV_OFF,     HSV_OFF,     HSV_OFF,     HSV_OFF
+    ),     
 
-              // Right hand
-    [MOUS] = {{0, 0, 0},     {0, 0, 0},     {0, 0, 0},     {0, 0, 0},     {0, 0, 0}, 
-              {HSV_MAGENTA}, {HSV_MAGENTA}, {HSV_MAGENTA}, {HSV_MAGENTA}, {0, 0, 0},
-              {HSV_PURPLE},  {HSV_PURPLE},  {HSV_PURPLE},  {HSV_PURPLE},  {0, 0, 0},
-              {HSV_BLUE},    {HSV_BLUE},    {HSV_BLUE},    {HSV_BLUE},    {0, 0, 0},
-                             {0, 0, 0},     {0, 0, 0},     {0, 0, 0},     {0, 0, 0},
+    [ARRW] = LED_LAYOUT_ergodox_ez_pretty(
+        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,
+        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,        HSV_OFF, HSV_OFF, HSV_RED, HSV_OFF, HSV_OFF,
+        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,        HSV_OFF, HSV_RED, HSV_RED, HSV_RED, HSV_OFF,
+        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,
+        HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF,                          HSV_OFF, HSV_OFF, HSV_OFF, HSV_OFF
+    ),
 
-              // Left Hand (flipped)
-              {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
-              {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
-              {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
-              {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
-                         {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
-
-              // Right hand
-    [ARRW] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
-              {0, 0, 0}, {0, 0, 0}, {HSV_RED}, {0, 0, 0}, {0, 0, 0},
-              {0, 0, 0}, {HSV_RED}, {HSV_RED}, {HSV_RED}, {0, 0, 0},
-              {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
-                         {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
-
-              // Left Hand (flipped)
-              {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
-              {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
-              {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
-              {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0},
-                         {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
-
-              // Right hand
-    [KTTY] = {{HSV_RED}, {HSV_RED},    {HSV_RED},    {HSV_RED},    {HSV_RED},
-              {0, 0, 0}, {HSV_PURPLE}, {0, 0, 0},    {HSV_PURPLE}, {0, 0, 0},
-              {0, 0, 0}, {0, 0, 0},    {0, 0, 0},    {HSV_BLUE},   {0, 0, 0},
-              {HSV_RED}, {0, 0, 0},    {HSV_BLUE},   {HSV_BLUE},   {0, 0, 0},
-                         {HSV_PURPLE}, {HSV_PURPLE}, {HSV_RED},    {HSV_RED},
-
-              // Left Hand (flipped)
-              {HSV_RED},  {HSV_RED},    {HSV_RED},    {HSV_RED},    {HSV_RED},
-              {HSV_BLUE}, {0, 0, 0},    {HSV_PURPLE}, {HSV_RED},    {HSV_BLUE},
-              {0, 0, 0},  {HSV_RED},    {0, 0, 0},    {HSV_PURPLE}, {0, 0, 0},
-              {HSV_RED},  {HSV_PURPLE}, {HSV_PURPLE}, {0, 0, 0},    {0, 0, 0},
-                          {HSV_BLUE},   {HSV_BLUE},   {0, 0, 0},    {HSV_RED}},
+    [KTTY] = LED_LAYOUT_ergodox_ez_pretty(
+        HSV_RED,  HSV_RED,    HSV_RED,    HSV_RED,    HSV_RED,        HSV_RED, HSV_RED,    HSV_RED,    HSV_RED,    HSV_RED,
+        HSV_BLUE, HSV_RED,    HSV_PURPLE, HSV_OFF,    HSV_BLUE,       HSV_OFF, HSV_PURPLE, HSV_OFF,    HSV_PURPLE, HSV_OFF,
+        HSV_OFF,  HSV_PURPLE, HSV_OFF,    HSV_RED,    HSV_OFF,        HSV_OFF, HSV_OFF,    HSV_OFF,    HSV_BLUE,   HSV_OFF,
+        HSV_OFF,  HSV_OFF,    HSV_PURPLE, HSV_PURPLE, HSV_RED,        HSV_RED, HSV_OFF,    HSV_BLUE,   HSV_BLUE,   HSV_OFF,
+        HSV_RED,  HSV_OFF,    HSV_BLUE,   HSV_BLUE,                            HSV_PURPLE, HSV_PURPLE, HSV_RED,    HSV_RED
+    ),
 
 };
 // clang-format on
