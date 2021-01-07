6decd5a3bba9f7b4ddd8116056563c67faf4a295
DERPSQUID9 <DERPSQUID@protonmail.com>
2021-01-06 19:39:02 -0500

    Enabled LTO and disabled certain unused features for Ploopy trackball mouse.

---
+++ keyboards/ploopyco/trackball/keymaps/Anomalocaridid/rules.mk
@@ -0,0 +1,9 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+
+# Enable desired features
+LTO_ENABLE = yes     # Enable Link Time Optimization
+
+# Disable unused/unwanted features
+CONSOLE_ENABLE = no  # Disable debugging console
+EXTRAKEY_ENABLE = no # Disable system control keys
