9dfe08163689d981f2d2f93ee8e67fdf3f166ba4
Anomalocaridid <DERPSQUID@protonmail.com>
2021-01-28 16:25:38 -0500

    Moved caps lock to Hyper Mod Tap

--- keyboards/ergodox_ez/keymaps/anomalocaridid/keymap.c
+++ keyboards/ergodox_ez/keymaps/anomalocaridid/keymap.c
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `----------------------'
  */
     [BASE] = LAYOUT_ergodox_pretty(
-        XXXXXXX, KC_1,   KC_2,    KC_3,    KC_4,    KC_5, RGB_MOD,                            TG(ARRW), KC_6, KC_7,  KC_8,    KC_9,    KC_0,    ALTCASE,
-        XXXXXXX, KC_Q,   KC_W,    KC_E,    KC_R,    KC_T, TT(FNCT),                           TG(MOUS), KC_Y, KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
-        XXXXXXX, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                               KC_H, KC_J,  KC_K,    KC_L,    KC_SCLN, KC_MINUS,
-        XXXXXXX, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, TT(KTTY),                           KC_DEL,   KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
-        KC_ESC,  KC_GRV, KC_QUOT, KC_LEFT, KC_RGHT,                                                           KC_UP, KC_DOWN, KC_LBRC, KC_RBRC, XXXXXXX,
-                                                          KC_HYPR,  KC_LGUI,         KC_RGUI, KC_MEH,
-                                                                    LALT_T(KC_CAPS), KC_RALT,
-                                         KC_SPACE, LCTL_T(KC_TAB),  KC_LSPO,         KC_RSPC, RCTL_T(KC_ENTER), KC_BSPC
+        XXXXXXX, KC_1,   KC_2,    KC_3,    KC_4,    KC_5, RGB_MOD,                           TG(ARRW), KC_6, KC_7,  KC_8,    KC_9,    KC_0,    ALTCASE,
+        XXXXXXX, KC_Q,   KC_W,    KC_E,    KC_R,    KC_T, TT(FNCT),                          TG(MOUS), KC_Y, KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
+        XXXXXXX, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                                              KC_H, KC_J,  KC_K,    KC_L,    KC_SCLN, KC_MINUS,
+        XXXXXXX, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, TT(KTTY),                          KC_DEL,   KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
+        KC_ESC,  KC_GRV, KC_QUOT, KC_LEFT, KC_RGHT,                                                          KC_UP, KC_DOWN, KC_LBRC, KC_RBRC, XXXXXXX,
+                                                          HYPR_T(KC_CAPS), KC_LGUI, KC_RGUI, KC_MEH,
+                                                                           KC_LALT, KC_RALT,
+                                                KC_SPACE, LCTL_T(KC_TAB),  KC_LSPO, KC_RSPC, RCTL_T(KC_ENTER), KC_BSPC
   ),
 
 /* Keymap 1: Function layer
