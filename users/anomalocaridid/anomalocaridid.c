216953835e605633e1f8ae69e1e866fd422baf40
Anomalocaridid <Duncan@anomalocaris.xyz>
2021-03-13 15:32:15 -0500

    Added Enter keypress to FLASH keycode macro.

--- users/anomalocaridid/anomalocaridid.c
+++ users/anomalocaridid/anomalocaridid.c
@@ -1,12 +1,9 @@
 #include "anomalocaridid.h"
 
-__attribute__((weak))
-bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
-    return true;
-}
+__attribute__((weak)) bool process_record_keymap(uint16_t keycode, keyrecord_t *record) { return true; }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch(keycode) {
+    switch (keycode) {
         case VRSN:
             if (record->event.pressed) {
                 SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
@@ -14,7 +11,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         case FLASH:
             if (record->event.pressed) {
-                SEND_STRING("qmk flash -kb " QMK_KEYBOARD " -km " QMK_KEYMAP);
+                SEND_STRING("qmk flash -kb " QMK_KEYBOARD " -km " QMK_KEYMAP SS_TAP(X_ENTER));
             }
             return false;
     }
